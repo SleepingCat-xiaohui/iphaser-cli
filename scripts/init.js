d35c3e8da0c46efd8fcf6afca1239195612eb679
shenghui.liu <shenghui.liu@zhaopin.com.cn>
2018-06-26 17:00:03 +0800

    Save

--- scripts/init.js
+++ scripts/init.js
@@ -1,5 +1,7 @@
 const shell = require('shelljs')
 const chalk = require('chalk')
+const fs = require('fs-extra')
+const path = require('path')
 const { config } = require('../.iphaser.config')
 
 if (!shell.which('git')) {
@@ -11,10 +13,22 @@ if (!shell.which('git')) {
  * init docs
  */
 Object.values(config).forEach(doc => {
-  shell.mkdir(doc.docPath)
+  const gitConfig = path.resolve(doc.docPath, '.git')
+  if (fs.existsSync(gitConfig)) fs.removeSync(gitConfig)
+
+  fs.ensureDirSync(doc.docPath)
   shell.cd(doc.docPath)
-  shell.exec('git init')
-  shell.exec(`git remote add origin ${doc.docRemote}`)
-  shell.exec('git config core.sparseCheckout true')
-  doc.docSparse.forEach(sparse => shell.exec(`echo ${sparse} >> .git/info/sparse-checkout`))
+  shell.exec('git init', { silent: true })
+  shell.exec(`git remote add origin ${doc.docRemote}`, { silent: true })
+  shell.exec('git config core.sparseCheckout true', { silent: true })
+  doc.docSparse.forEach(sparse => shell.exec(`echo ${sparse} >> .git/info/sparse-checkout`), { silent: true })
 })
+
+/**
+ * say hello
+ */
+const helloMessage = chalk.cyan(`  Hi guy~ Do you ❤ Phaser?
+  If you do, you can join the Phaser QQ group(519413640), You'll find the big guy, the middle guy, the little guy and the gay guy~
+  Looking forward to your participation.
+`)
+console.log(helloMessage)
